c84c5ee26bab07ce032b420d90050dd85d635500
mathildefpsn <mathilde.person@ynov.com>
2020-03-06 12:23:59 +0100

    leapyears should_return_true_if_divisible_by_4_and_not_by_100 GREEN

--- src/main/java/LeapYears.java
+++ src/main/java/LeapYears.java
@@ -1,7 +1,7 @@
 public class LeapYears {
 
     public Boolean isLeapYear(Integer year) {
-        if (year % 400 == 0) return true;
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) return true;
         return false;
     }
 }
