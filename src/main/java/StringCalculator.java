72fe1a607950043876ce6cf7559ab1423fe1116f
mathildefpsn <mathilde.person@ynov.com>
2020-03-06 18:41:22 +0100

    stringcalculator REFACTO

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -7,31 +7,39 @@ import java.util.stream.Collectors;
 
 public class StringCalculator {
 
+    private static final String BAD_SEPARATOR_1 = ",\n";
+    private static final String BAD_SEPARATOR_2 = "\n,";
+    private static final String BAD_SEPARATOR_1_TO_PRINT = ",\\n";
+    private static final String BAD_SEPARATOR_2_TO_PRINT = "\\n,";
+    private static final String SEPARATORS = "[(,)|(\\n)]";
+
     String add(String ...number) {
-        List<List<String>> args = Arrays.stream(number)
+        List<List<String>> numbersLists = Arrays.stream(number)
                 .filter(num -> !num.isEmpty())
                 .map(num -> {
                     try {
-                        String badSeparator1 = ",\n";
-                        String badSeparator2 = "\n,";
-                        if (num.contains(badSeparator1)) {
-                            throw new NumberExpectedException(num.indexOf(badSeparator1),",\\n");
-                        } else if (num.contains(badSeparator2)) {
-                            throw new NumberExpectedException(num.indexOf(badSeparator2),"\\n,");
-                        }
-                        String[] numbersArray = num.split("[(,)|(\\n)]");
-                        if (String.valueOf(num.charAt(num.length() - 1)).matches("[(,)|(\\n)]")) throw new MissingNumberException();
-                        return Arrays.asList(numbersArray);
+                        this.checkSeparator(num);
+                        return Arrays.asList(num.split(SEPARATORS));
                     } catch (NumberExpectedException | MissingNumberException e) {
                         throw e;
                     }
                 })
                 .collect(Collectors.toList());
-        if (args.size() == 0) return "0";
-        return String.valueOf(args.stream()
+        if (numbersLists.size() == 0) return "0";
+        return String.valueOf(numbersLists.stream()
                 .flatMap(List::stream)
                 .filter(num -> !num.isEmpty())
                 .mapToDouble(Double::valueOf)
                 .sum());
+    }
+
+    private void checkSeparator(String numbers) {
+        if (numbers.contains(BAD_SEPARATOR_1)) {
+            throw new NumberExpectedException(numbers.indexOf(BAD_SEPARATOR_1), BAD_SEPARATOR_1_TO_PRINT);
+        } else if (numbers.contains(BAD_SEPARATOR_2)) {
+            throw new NumberExpectedException(numbers.indexOf(BAD_SEPARATOR_2), BAD_SEPARATOR_2_TO_PRINT);
+        } else if (String.valueOf(numbers.charAt(numbers.length() - 1)).matches(SEPARATORS)) {
+            throw new MissingNumberException();
+        }
     }
 }
--- src/main/java/exceptions/NumberExpectedException.java
+++ src/main/java/exceptions/NumberExpectedException.java
@@ -1,7 +1,10 @@
 package exceptions;
 
 public class NumberExpectedException extends RuntimeException {
+
     public NumberExpectedException(Integer position, String separator) {
         super("Number expected but '" + separator + "' found at position " + position + ".");
     }
+
+
 }
