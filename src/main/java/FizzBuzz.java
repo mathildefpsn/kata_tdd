725e49104ee33fa71daad1a4b831325fed1b4210
mathildefpsn <mathilde.person@ynov.com>
2020-03-06 12:03:38 +0100

    fizzbuzz should_return_fizzbuzz_if_divisible_by_5_and_3 GREEN

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,9 @@
 public class FizzBuzz {
 
     public String getPrintValue(Integer number) {
-        if (number % 3 == 0) {
+        if ((number % 3 == 0) && (number % 5 == 0)) {
+            return "fizzbuzz";
+        } else if (number % 3 == 0) {
             return "fizz";
         } else if (number % 5 == 0) {
             return "buzz";
