afa51cc6843e908ed98dec66246d9be33f7eee60
mathildefpsn <mathilde.person@ynov.com>
2020-03-06 12:25:37 +0100

    leapyears REFACTO

--- src/main/java/LeapYears.java
+++ src/main/java/LeapYears.java
@@ -1,7 +1,6 @@
 public class LeapYears {
 
     public Boolean isLeapYear(Integer year) {
-        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) return true;
-        return false;
+        return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
     }
 }
