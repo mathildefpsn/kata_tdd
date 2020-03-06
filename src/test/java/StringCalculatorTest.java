3cd783ae6a25a186d562a75e7180c49b98d3ce19
mathildefpsn <mathilde.person@ynov.com>
2020-03-06 12:39:05 +0100

    stringcalculator should_return_0_if_input_is_empty_string RED

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -12,4 +12,11 @@ public class StringCalculatorTest {
         assertEquals("6.2", stringCalculator.add(numbers));
     }
 
+    @Test
+    void should_return_0_if_input_is_empty_string() {
+        String numbers = "";
+
+        assertEquals("0", stringCalculator.add(numbers));
+    }
+
 }
