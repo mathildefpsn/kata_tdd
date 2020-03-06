adea6d3097f5e355c6f8d4e6aa713c4fad6d88c1
mathildefpsn <mathilde.person@ynov.com>
2020-03-06 14:38:21 +0100

    stringcalculator REFACTO

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,18 +5,13 @@ import java.util.stream.Collectors;
 public class StringCalculator {
 
     String add(String ...number) {
-        if (number.length == 0) return "0";
         List<List<String>> args = Arrays.stream(number)
                 .filter(num -> !num.isEmpty())
                 .map(num -> Arrays.asList(num.split(",")))
                 .collect(Collectors.toList());
         if (args.size() == 0) return "0";
-        List<String> numbers =
-                args.stream()
-                        .flatMap(List::stream)
-                        .collect(Collectors.toList());
-
-        return String.valueOf(numbers.stream()
+        return String.valueOf(args.stream()
+                .flatMap(List::stream)
                 .mapToDouble(Double::valueOf)
                 .sum());
     }
