4f74c1323d37b859707b5942b2bbc88d834423e5
mathildefpsn <mathilde.person@ynov.com>
2020-03-06 12:42:27 +0100

    stringcalculator should_return_0_if_input_is_empty_string GREEN

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class StringCalculator {
 
     String add(String number) {
+        if (number.isEmpty()) return "0";
         String[] numbers = number.split(",");
         double sum = 0;
         for (String num: numbers) {
