0fc15fe27b0691a9b1e383414ce2b2042712c95d
mathildefpsn <mathilde.person@ynov.com>
2020-03-06 14:34:27 +0100

    stringcalculator should_return_sum_of_numbers_with_an_unknown_number_of_arguments GREEN

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,11 +1,21 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
-    String add(String number) {
-        if (number.isEmpty()) return "0";
-        List<String> numbers = Arrays.asList(number.split(","));
+    String add(String ...number) {
+        if (number.length == 0) return "0";
+        List<List<String>> args = Arrays.stream(number)
+                .filter(num -> !num.isEmpty())
+                .map(num -> Arrays.asList(num.split(",")))
+                .collect(Collectors.toList());
+        if (args.size() == 0) return "0";
+        List<String> numbers =
+                args.stream()
+                        .flatMap(List::stream)
+                        .collect(Collectors.toList());
+
         return String.valueOf(numbers.stream()
                 .mapToDouble(Double::valueOf)
                 .sum());
