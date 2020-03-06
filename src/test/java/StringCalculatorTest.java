42cda2f3988bb23ec0b27ff4ee11d129b6c7e3ee
mathildefpsn <mathilde.person@ynov.com>
2020-03-06 13:01:10 +0100

    stringcalculator should_return_sum_of_numbers_with_an_unknown_number_of_arguments RED

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
@@ -17,6 +20,13 @@ public class StringCalculatorTest {
         String numbers = "";
 
         assertEquals("0", stringCalculator.add(numbers));
+    }
+
+    @Test
+    void should_return_sum_of_numbers_with_an_unknown_number_of_arguments() {
+        String[] numbers = new String[]{"", "1.2,2,3", "2"};
+
+        assertEquals("8.2", stringCalculator.add(numbers));
     }
 
 }
