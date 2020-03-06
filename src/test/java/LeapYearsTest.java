fae8fab5e0194d0fc251ca5f0b644d7f8af2bc06
mathildefpsn <mathilde.person@ynov.com>
2020-03-06 12:22:49 +0100

    leapyears should_return_true_if_divisible_by_4_and_not_by_100 RED

--- src/test/java/LeapYearsTest.java
+++ src/test/java/LeapYearsTest.java
@@ -11,8 +11,15 @@ public class LeapYearsTest {
         assertEquals(true, leapYears.isLeapYear(a));
     }
 
+    @Test
     void should_return_false_if_divisible_by_100_and_not_by_400() {
         Integer a = 1700;
         assertEquals(false, leapYears.isLeapYear(a));
+    }
+
+    @Test
+    void should_return_true_if_divisible_by_4_and_not_by_100() {
+        Integer a = 2008;
+        assertEquals(true, leapYears.isLeapYear(a));
     }
 }
